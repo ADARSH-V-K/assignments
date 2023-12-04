e4988e3ae899ae8ea56597fae682e7ccf4bc0175
ADARSH-V-K <120708977+ADARSH-V-K@users.noreply.github.com>
2023-12-04 14:00:04 +0530

    Update anagram.js
    a program to find whether two strings are anagrams

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,6 +5,28 @@
 */
 
 function isAnagram(str1, str2) {
+      let j
+    if(str1.length!=str2.length){
+        console.log('they are not anagrams');
+    }else{
+
+    
+    for(let i=0;i<str1.length;i++){
+        for(let j=0;j<str2.length;j++){
+            if(str1[i]===str2[j]){
+                str2[j]=0 
+            }
+        }
+        if(j>str2.length){
+            console.log('they are not anagrams');
+        }
+
+    }
+    console.log('they are anagrams');
+}
+}
+isAnagram('hey you herte','yeh you there')
+
 
 }
 
